6fe9ee3fd233c28952601845164a094ba4f51f6c
Vanya Sergeev <vsergeev@gmail.com>
2014-01-25 19:01:11 -0800

    modify test helpers to work with string pointers

--- test.h
+++ test.h
@@ -3,9 +3,9 @@
 
 #include "debug.h"
 
-#define STR_OK          "[\x1b[1;32m OK \x1b[0m]"
-#define STR_FAIL        "[\x1b[1;31mFAIL\x1b[0m]"
-#define STR_INTERACT    "[\x1b[1;36m    \x1b[0m]"
+#define STR_OKAY        " [\x1b[1;32m OK \x1b[0m]  "
+#define STR_FAIL        " [\x1b[1;31mFAIL\x1b[0m]  "
+#define STR_TAB         "         "
 
 #ifdef GIT_VERSION
 #define STR_VERSION     "\x1b[1;33m" GIT_VERSION "\x1b[0m"
@@ -17,32 +17,41 @@
     do { \
         int r = (c); \
         if (r) \
-            debug_printf(" " STR_OK "  %s():%d  %s\n", __func__, __LINE__, #c); \
+            debug_printf(STR_OKAY "%s():%d  %s\n", __func__, __LINE__, #c); \
         else {\
-            debug_printf(" " STR_FAIL "  %s():%d  %s\n", __func__, __LINE__, #c); \
+            debug_printf(STR_FAIL "%s():%d  %s\n", __func__, __LINE__, #c); \
             return; \
         } \
     } while(0)
 
 #define pokay(fmt, ...) \
-    debug_printf(" " STR_OK "  " fmt "\n", ##__VA_ARGS__)
+    do { \
+        uart_puts(STR_OKAY); \
+        debug_printf(fmt, ##__VA_ARGS__); \
+        uart_puts("\n"); \
+    } while (0)
 
 #define pfail(fmt, ...) \
-    debug_printf(" " STR_FAIL "  " fmt "\n", ##__VA_ARGS__)
+    do { \
+        uart_puts(STR_FAIL); \
+        debug_printf(fmt, ##__VA_ARGS__); \
+        uart_puts("\n"); \
+    } while (0)
 
 #define ptest() \
     debug_printf("\nStarting test %s() (%s:%d)\n", __func__, __FILE__, __LINE__)
 
 #define pinteract(fmt, ...) \
     { \
-        char c; \
-        debug_printf(" " STR_INTERACT "  " fmt "? (y/n/r) ", ##__VA_ARGS__); \
-        c = uart_getc(); \
-        uart_putc('\n'); \
-        if (c == 'y' || c == 'Y') { \
+        char _buf[2]; \
+        uart_puts(STR_TAB); \
+        debug_printf(fmt, ##__VA_ARGS__); \
+        uart_puts(" (y/n/r) "); \
+        uart_gets(_buf, sizeof(buf), true); \
+        if (_buf[0] == 'y' || _buf[0] == 'Y') { \
             pokay(fmt, ##__VA_ARGS__); \
             break; \
-        } else if (c == 'n' || c == 'N') { \
+        } else if (_buf[0] == 'n' || _buf[0] == 'N') { \
             pfail(fmt, ##__VA_ARGS__); \
             break; \
         } else { \
